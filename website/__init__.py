cd74fff41927f8aabf364ba67efedbf6e64f96ed
Nate <nate.pardee@gmail.com>
2023-06-14 11:59:47 -0500

    issues running code

--- website/__init__.py
+++ website/__init__.py
@@ -12,12 +12,14 @@ def create_app():
     db.init_app(app)
 
     from .views import views
-    from .auth import auth
+    from .auth import auth  
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
+
+    create_database(app)
 
     return app
 
@@ -25,3 +27,4 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
+
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
 
@@ -31,6 +34,11 @@ def sign_up():
             flash('Passwords don\'t match.', category='error')      
      
         else:
+           # new_user = User(email=email, firstName=firstName, #password=generate_password_hash(password1, method='sha256'))
+            #db.session.add(new_user)
+           # db.session.commit()
+
             flash('Account successfully created!',category='success')
+            #return redirect(url_for('views.home'))
             
     return render_template("sign_up.html")
