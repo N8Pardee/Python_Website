e97c91241b712f8cccec1aa7a4756750cdba4bbf
Nate Pardee <109477225+N8Pardee@users.noreply.github.com>
2023-06-14 12:17:51 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 auth = Blueprint('auth', __name__)
